bc1e56417db7cedb11f70831e3e50bd09d5b92ab
Sneha Naphade <sneha.naphade833@gmail.com>
2021-10-30 16:58:24 +0530

    gcd program added

---
+++ GCD.cpp
@@ -0,0 +1,24 @@
+//C++ program to calculate GCD of 2 numbers
+
+#include<iostream>
+using namespace  std;
+
+int main()
+{
+    int num1,num2,gcd;
+    cout<<"Enter First Number: ";
+    cin>>num1;
+    cout<<"Enter Seconf Number: ";
+    cin>>num2;
+    for(int i=1;i<=num1 && i<=num2;i++)
+    {
+        if(num1%i==0 && num2%i==0)
+        {
+            gcd=i;
+        }
+    }
+    cout<<"GCD= "<<gcd;
+    return 0;
+
+}
+
